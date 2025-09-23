4998f6cdf94451a8345b382879af6266f12ba8f5
Vedant <vedant.kalia@clouddrove.com>
2025-09-23 22:37:58 +0530

    fix: Updated Workflow and Fixed Attributes

--- examples/auto-shutdown/outputs.tf
+++ examples/auto-shutdown/outputs.tf
@@ -23,7 +23,3 @@ output "virtual_machine_id" {
   description = "The ID of the Virtual Machine."
 }
 
-output "tags" {
-  value       = module.virtual-machine.tags
-  description = "The tags associated to resources."
-}
